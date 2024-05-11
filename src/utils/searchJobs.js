22d4201cc30df2fc1d6e8d927d69421d78d0a965
Gabriel Rodrigues <gabers357@gmail.com>
2024-05-11 13:40:35 -0300

    Refactor puppeteer code for job search functionality and error handling

--- src/utils/searchJobs.js
+++ src/utils/searchJobs.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import { urlList } from "../../link.js";
 import fs from "fs";
+import path from "path";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 async function searchJobs(searchQuery) {
@@ -10,7 +11,7 @@ async function searchJobs(searchQuery) {
     userDataDir: "/tmp/myChromeSession",
   });
 
-  let allJobs = [];
+  let newJobs = [];
 
   try {
     const page = await browser.newPage();
@@ -20,7 +21,8 @@ async function searchJobs(searchQuery) {
       "fullstack",
       "devops",
       "mobile",
-      // searchQuery,
+      "junior",
+      searchQuery,
     ];
 
     for (const url of urlList) {
@@ -40,21 +42,16 @@ async function searchJobs(searchQuery) {
             );
             return itens.map((li) => {
               const nomeVaga = li.querySelector(
-                "div>  div:nth-child(1)"
+                "div> div:nth-child(1)"
               )?.innerText;
               const modeloVaga = li.querySelector(
-                "div>  div:nth-child(2)"
+                "div> div:nth-child(2)"
               )?.innerText;
               const linkVaga = li.querySelector(
                 'a[data-testid="job-list__listitem-href"]'
               )?.href;
               const data = new Date().toISOString();
-              return {
-                nomeVaga,
-                linkVaga,
-                modeloVaga,
-                data,
-              };
+              return { nomeVaga, linkVaga, modeloVaga, data };
             });
           });
           delay(6);
@@ -66,20 +63,46 @@ async function searchJobs(searchQuery) {
         }
       }
 
-      allJobs.push(companyJobs);
+      newJobs.push(companyJobs);
     }
   } catch (err) {
     console.error(`Erro geral: ${err}`);
   } finally {
     await browser.close();
-    fs.writeFile("jobResults.json", JSON.stringify(allJobs, null, 2), (err) => {
-      if (err) {
-        console.error("Erro ao salvar o arquivo:", err);
-      } else {
-        console.log("Dados salvos em jobResults.json");
+
+    const dir = path.dirname(new URL(import.meta.url).pathname);
+    const filePath = path.join(dir, "jobResults.json");
+    fs.readFile(filePath, (err, data) => {
+      let existingJobs = err ? [] : JSON.parse(data.toString());
+
+      for (let newJob of newJobs) {
+        let existingCompany = existingJobs.find(
+          (job) => job.empresa === newJob.empresa
+        );
+        if (existingCompany) {
+          for (let newVaga of newJob.vagas) {
+            let vagaExists = existingCompany.vagas.some(
+              (vaga) =>
+                vaga.nomeVaga === newVaga.nomeVaga &&
+                vaga.linkVaga === newVaga.linkVaga
+            );
+            if (!vagaExists) {
+              existingCompany.vagas.push(newVaga);
+            }
+          }
+        } else {
+          existingJobs.push(newJob);
+        }
       }
+
+      fs.writeFile(filePath, JSON.stringify(existingJobs, null, 2), (err) => {
+        if (err) {
+          console.error("Erro ao salvar o arquivo:", err);
+        } else {
+          console.log("Dados atualizados salvos em jobResults.json");
+        }
+      });
     });
   }
 }
-// searchJobs();
 export default searchJobs;
